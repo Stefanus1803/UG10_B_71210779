40f2080e55694b157aae7ba2cde0d84fd5cb087f
Stefanus <93358497+Stefanus1803@users.noreply.github.com>
2022-11-24 14:41:57 +0700

    Commit soal 2

--- 2_B_71210779.py
+++ 2_B_71210779.py
@@ -1,4 +1,9 @@
-print("="*10,"PROGRAM PENGHITUNG PYTHAGORAS","="*10)
-bilangan1 = int(input("Masukan bilangan bulat pertama: "))
-bilangan2 = int(input("Masukan bilangan bulat kedua: "))
-bilangan3 = int(input("Masukan bilangan bulat ketiga: "))
+print ("="*10, "PROGRAM PENGHITUNG PYTHAGORAS", "="*10)
+bilangan1 = int(input("Masukkan bilangan bulat pertama: "))
+bilangan2 = int(input("Masukkan bilangan bulat kedua: "))
+bilangan3 = int(input("Masukkan bilangan bulat ketiga: "))
+penjumlahan = ((bilangan1**2) + (bilangan2**2))
+if penjumlahan == (bilangan3**2):
+    print ("Merupakan Pythagoras.")
+else:
+    print("Bukan merupakan Pythagoras.")
